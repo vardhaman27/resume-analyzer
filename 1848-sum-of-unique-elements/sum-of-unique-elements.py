6d949d678b18b4d48c37fea80f4b396300d89a82
Vardhaman Jain <vardhaman0270@gmail.com>
2026-08-27 13:57:50 +0530

    Time: 3 ms (24.19%) | Memory: 12.3 MB (56.63%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -4,7 +4,7 @@ class Solution(object):
         for i in nums:
             mapp[i] = mapp.get(i,0) + 1
         add= 0
-        for key, value in mapp.items():
-            if value == 1:
-                add += key
+        for value in mapp:
+                if mapp[value] == 1:
+                    add += value
         return add
